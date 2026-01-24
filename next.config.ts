cbbe559d4085d02443ddec42cb8346fce5593dbd
a.s. cartoon <jagtarsingh93033@gmail.com>
2026-01-24 09:33:18 +0000

    "I want to manually deploy this app to Netlify by uploading a ZIP file.

--- next.config.ts
+++ next.config.ts
@@ -3,30 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  output: 'standalone',
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh',
-      },
-       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
