0434a44e69f6b99084cb0308c6938b5f60100765
a.s. cartoon <jagtarsingh93033@gmail.com>
2026-01-25 02:32:07 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://res.clo

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
